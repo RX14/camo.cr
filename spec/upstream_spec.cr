006dcf9055f37a862b68fe5aad7e85f5116b5770
RX14 <chris@rx14.co.uk>
2018-11-10 22:53:15 +0000

    Run upstream specs in a spec block

--- spec/upstream_spec.cr
+++ spec/upstream_spec.cr
@@ -1,13 +1,17 @@
 require "../src/camo"
 
-camo_key = "0x24FEEDFACEDEADBEEFCAFE"
-config = Camo::Config.new(camo_key)
+describe Camo do
+  it "runs upstream tests" do
+    camo_key = "0x24FEEDFACEDEADBEEFCAFE"
+    config = Camo::Config.new(camo_key)
 
-spawn { Camo.new(config).run }
+    spawn { Camo.new(config).run }
 
-Process.run(
-  "rake", args: ["bundle", "test"],
-  env: {"CAMO_KEY" => camo_key},
-  output: Process::Redirect::Inherit, error: Process::Redirect::Inherit,
-  chdir: File.join(__DIR__, "upstream")
-)
+    Process.run(
+      "rake", args: ["bundle", "test"],
+      env: {"CAMO_KEY" => camo_key},
+      output: Process::Redirect::Inherit, error: Process::Redirect::Inherit,
+      chdir: File.join(__DIR__, "upstream")
+    )
+  end
+end
